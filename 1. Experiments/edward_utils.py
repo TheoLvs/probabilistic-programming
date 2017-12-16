a0a60cac2c86927fa3241599dd6e856cacb4741d
Theo Alves Da Costa <theo.alves.da.costa@gmail.com>
2017-12-16 01:04:48 +0100

    edward utils and update notebook with gaussian fitting

---
+++ 1. Experiments/edward_utils.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+
+"""--------------------------------------------------------------------
+EDWARD UTILS
+Started on the 16/12/2017
+theo.alves.da.costa@gmail.com
+https://github.com/theolvs
+------------------------------------------------------------------------
+"""
+
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from tqdm import tqdm
+from scipy import stats
+
+# Probabilistic programming
+import tensorflow as tf
+import edward as ed
+from edward.models import Bernoulli,Beta,Empirical,Normal,Poisson,Uniform,Exponential
+
+
+
+
+
+class Distribution(object):
+    def __init__(self):
+        pass
+
+
+
+
+
+class Problem(object):
+    def __init__(self):
+        pass
+
+
+
